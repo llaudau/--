986d01fc97322bb886a1e6cf81d1bfa901296ce0
llaudau <wangkehe06@gmail.com>
2025-09-30 12:30:25 +0800

    ass1

--- Phy_Compute/prog1/main.cpp
+++ Phy_Compute/prog1/main.cpp
@@ -6,10 +6,14 @@
 #include "sum_ispc.h"
 void serialsum(double x, int i, double result[] ){
     double partial=0.0;
-    for(int j=1;j< 1000000000;j++){
-        partial+=1/(j*(j+x));
+    double c=0.0;
+    for(int j=1;j< 10000000;j++){
+        double term=1/(j*(j+x))-c;
+        double t=partial+term;
+        c=(t-partial)-term;
+        partial=t;
     }
-    result[i]=partial;
+    result[i]=partial+c;
 };
 int main() {
     double  x1=pow(2,1.f/2);
@@ -17,14 +21,11 @@ int main() {
     double x[N]={0.f,0.5f,1.f,x1,100.f,300.f};
     double result[N];
     for(int i=0;i<N;i++){
-        
-        auto start = std::chrono::high_resolution_clock::now();
-        // serialsum(x[i],i,result);
-        ispc::sum1(x[i],i,result);
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        std::cout << "Time taken: " << duration.count() << " milliseconds" << std::endl;
 
+        serialsum(x[i],i,result);
+    
         std::cout<<"x="<<x[i]<<", psi(x) = "<<std::setprecision(10)<<result[i]<<std::endl;
+        // ispc::sum1(x[i],i,result);
+        std::cout<<"ispc: "<<"x="<<x[i]<<", psi(x) = "<<std::setprecision(10)<<result[i]<<std::endl;
     }
     return 0;}
