a2a2512ac575a8edbadfbe4cd17830014373ba2a
llaudau <wangkehe06@gmail.com>
2025-10-20 16:45:24 +0800

    qcd

---
+++ qcd/config_generate/build/CMakeFiles/config_gen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/config_gen.dir/main.cpp.o"
+  "CMakeFiles/config_gen.dir/main.cpp.o.d"
+  "config_gen"
+  "config_gen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/config_gen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ qcd/config_generate/build/lattice.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <unsupported/Eigen/CXX11/Tensor>
+#include <complex>
+using namespace Eigen;
+
+#pragma once
+
+class lattice{
+    private:
+    MatrixXd gauge_field=;
+    public:
+    lattice(size);
+}
